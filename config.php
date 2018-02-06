cad8a08fdf327c4355b3d396bfe904731a095bd7
justinswan <justin.swan@aamcommercial.com.au>
2018-02-06 21:17:13 +1000

    Initial crypt commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$config = [
+    'password' => 'humpdedoo'
+];
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include "config.php";
+echo "Hello world, how's it crypting";
+echo "\nMy password is:" . $config['password'];
